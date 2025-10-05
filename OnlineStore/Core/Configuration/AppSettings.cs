0579d02b5d2605a32da39773f1a4421fb626e614
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-05 17:52:04 +0300

    Add basic classes for configuration

---
+++ OnlineStore/Core/Configuration/AppSettings.cs
@@ -0,0 +1,47 @@
+﻿namespace OnlineStore.Core.Configuration
+{
+	/**
+	 * App settings is basically a collection of configurations. Each configuration
+	 * is represented by IConfig.
+	 * 
+	 * Will get injected as a service in the controllers.
+	 */
+	public class AppSettings
+	{
+		/**
+		 * One one hand, you want to store all configurations in the same place.
+		 * But on the other hand, each configuration is usually accessed by type,
+		 * so you want to provide easy access using the type, without having
+		 * to search over the whole list.
+		 */
+		private readonly Dictionary<Type, IConfig> configs; // no duplications allowed
+
+		public AppSettings(IList<IConfig>? configs = null)
+		{
+			this.configs = configs
+				?.OrderBy(config => config.GetOrder())
+				?.ToDictionary(config => config.GetType(), config => config)
+				?? new Dictionary<Type, IConfig>();
+		}
+
+		// Get a configuration by its type.
+		public TConfig Get<TConfig>() where TConfig : class, IConfig
+		{
+			if (configs[typeof(TConfig)] is not TConfig config)
+			{
+				throw new Exception($"No configuration with type '{typeof(TConfig)}' found");
+			}
+
+			return config;
+		}
+
+		// Update the list of configurations.
+		public void Update(IList<IConfig> configs)
+		{
+			foreach (var config in configs)
+			{
+				this.configs[config.GetType()] = config;
+			}
+		}
+	}
+}
---
+++ OnlineStore/Core/Configuration/IConfig.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineStore.Core.Configuration
+{
+	public interface IConfig
+	{
+		/**
+		 * Each configuration has a section name.
+		 * 
+		 * The name is the same as the type name (say, HostingConfig).
+		 */
+		string Name => GetType().Name;
+
+		public int GetOrder() => 1;
+	}
+}
