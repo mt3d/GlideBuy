f0eda1db327827b059b360fbfca7545e71c26e42
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-07 11:18:31 +0300

    Add ShippingStatus enum

---
+++ OnlineStore/Core/Domain/Shipping/ShippingStatus.cs
@@ -0,0 +1,15 @@
+﻿namespace OnlineStore.Core.Domain.Shipping
+{
+	public enum ShippingStatus
+	{
+		ShippingNotRequire = 10,
+
+		NotShippedYet = 20,
+
+		PartiallyShipped = 30,
+
+		Shipped = 40,
+
+		Delivered = 50
+	}
+}
