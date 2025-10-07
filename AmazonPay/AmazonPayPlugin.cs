5fb064eb99cce58a58726cca55d8b6adda0c36d7
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-07 14:09:34 +0300

    Add an initital AmazonPay plugin

---
+++ AmazonPay/AmazonPayPlugin.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.Services.Payments;
+
+namespace GlideBuy.Plugin.Payments.AmazonPay
+{
+	public class AmazonPayPlugin : IPaymentMethod
+	{
+		public Task<ProcessPaymentResult> ProcessPaymentAsync(ProcessPaymentRequest processPaymentRequest)
+		{
+			/**
+			 * When you use a payment method that redirects to a third-party
+			 * site (like PayPal Standard), there is no need to do any processing.
+			 * The real processing happens after the redirection.
+			*/
+			return Task.FromResult(new ProcessPaymentResult());
+		}
+	}
+}
