36a375ef823a07398b0c361e4a1045a177c852ad
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-09 20:39:49 +0300

    Handle the shopping cart using an MCV controller instead of a Razor Page

---
+++ OnlineStore/Controllers/ShoppingCartController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Models;
+using OnlineStore.Models.ShoppingCart;
+using OnlineStore.Services.ProductCatalog;
+
+namespace OnlineStore.Controllers
+{
+	public class ShoppingCartController : Controller
+	{
+		private IProductService productService;
+
+		public ShoppingCartController(
+			IProductService productService,
+			Cart cartService) // The cart stored in the session is added as a scoped service
+		{
+			this.productService = productService;
+			CartService = cartService;
+		}
+
+		public Cart CartService { get; set; }
+
+		public IActionResult Cart(string returnUrl)
+		{
+			return View("Cart", new ShoppingCartModel { ReturnUrl = returnUrl ?? "/", Cart = CartService });
+		}
+
+		[HttpPost]
+		public IActionResult AddProduct(long productId, string returnUrl)
+		{
+			Product? product = productService.GetProductById(productId);
+
+			if (product != null)
+			{
+				CartService.AddItem(product, 1);
+			}
+
+			return RedirectToAction("Cart", new { returnUrl = returnUrl });
+		}
+
+		[HttpPost]
+		public IActionResult RemoveProduct(long productId, string returnUrl)
+		{
+			CartService.RemoveLine(CartService.Lines.First(cl => cl.Product.ProductId == productId).Product);
+
+			return RedirectToAction("Cart", new { returnUrl = returnUrl });
+		}
+	}
+}
---
+++ OnlineStore/Models/ShoppingCart/ShoppingCartModel.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.Models.ShoppingCart
+{
+	public class ShoppingCartModel
+	{
+		public string ReturnUrl { get; set; } = "/";
+
+		public Cart? Cart { get; set; }
+	}
+}
