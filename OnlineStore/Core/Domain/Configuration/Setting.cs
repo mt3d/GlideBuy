a6c29d85aab899e9c8f4be13634c6c4c0165ace5
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-14 01:06:17 +0300

    Add a Setting class

---
+++ OnlineStore/Core/Domain/Configuration/Setting.cs
@@ -0,0 +1,26 @@
+﻿namespace GlideBuy.Core.Domain.Configuration
+{
+	public class Setting
+	{
+		public Setting() { }
+
+		public Setting(string name, string value)
+		{
+			Name = name;
+			Value = value;
+		}
+
+		public int SettingId { get; set; }
+
+		public string Name { get; set; }
+
+		public string Value { get; set; }
+
+		//public int StoreId { get; set; }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
