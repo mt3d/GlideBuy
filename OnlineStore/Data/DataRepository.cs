8f2109f710a1a56ff1a704958e5c1446310608b6
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-15 22:22:46 +0300

    Add ReturnDeleted() to DataRepository

--- OnlineStore/Data/DataRepository.cs
+++ OnlineStore/Data/DataRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using OnlineStore.Data;
+using OnlineStore.Models.Common;
 
 namespace GlideBuy.Data
 {
@@ -17,6 +18,25 @@ namespace GlideBuy.Data
 			 * each time that it is called for a given context instance and entity type.
 			 */
 			Table = context.Set<T>();
+		}
+
+		/// <summary>
+		/// Determines if soft deleted entities should be returned,
+		/// When the query applies to <see cref="ISoftDeletable"/> entities.
+		/// </summary>
+		/// <param name="query"></param>
+		/// <param name="includeDeleted"></param>
+		/// <returns></returns>
+		private IQueryable<T> ReturnDeleted(IQueryable<T> query, in bool includeDeleted)
+		{
+			// By default, EF Core queries will return "soft deleted" entities.
+			// If the class do not implement ISoftDeletable, then do nothing.
+			if (includeDeleted || typeof(T).GetInterface(nameof(ISoftDeletable)) == null)
+			{
+				return query;
+			}
+
+			return query.OfType<ISoftDeletable>().Where(row => row.Deleted != true).OfType<T>();
 		}
 	}
 }
