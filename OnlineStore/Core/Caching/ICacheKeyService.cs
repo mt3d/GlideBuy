fa16fb21ccec8ba1bf9e5bb766d7ff171d952dd7
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-16 09:44:26 +0300

    Add ICacheKeyService interface

---
+++ OnlineStore/Core/Caching/ICacheKeyService.cs
@@ -0,0 +1,20 @@
+﻿namespace GlideBuy.Core.Caching
+{
+	/// <summary>
+	/// A cache key service interface.
+	/// </summary>
+	public interface ICacheKeyService
+	{
+		/**
+		 * Takes an initial CacheKey object (which defines a base key string, e.g. "language.all")
+		 * Combines it with parameters (like storeId, showHidden, etc.)
+		 * Returns a new CacheKey instance that includes those parameters.
+		 */
+		CacheKey PrepareKey(CacheKey key, params object[] cacheKeyParams);
+
+		/**
+		 * Same as above, but assign the default cache duration.
+		 */
+		CacheKey PrepareKeyForDefaultCache(CacheKey key, params object[] cacheKeyParams);
+	}
+}
