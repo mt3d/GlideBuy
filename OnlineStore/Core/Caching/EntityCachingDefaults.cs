3be7f1fe6211f362d2b362fe04c4531b3ee5a9d4
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-10-20 16:14:21 +0300

    Add EntityCachingDefaults for storing default cache values of entities

---
+++ OnlineStore/Core/Caching/EntityCachingDefaults.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Models;
+
+namespace GlideBuy.Core.Caching
+{
+	public class EntityCachingDefaults<T> where T : BaseEntity
+	{
+		public static string EntityTypeName => typeof(T).Name.ToLowerInvariant();
+
+		public static CacheKey AllCacheKey => new($"GlideBuy.{EntityTypeName}.all");
+	}
+}
