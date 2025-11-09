1b054038635a4adc5a8f061049501bd8560180f3
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-11-09 16:46:10 +0300

    Add an address model

--- OnlineStore/Web/Models/Checkout/CheckoutBillingAddressModel.cs
+++ OnlineStore/Web/Models/Checkout/CheckoutBillingAddressModel.cs
@@ -1,7 +1,10 @@
-﻿namespace GlideBuy.Web.Models.Checkout
+﻿using GlideBuy.Web.Models.Common;
+
+namespace GlideBuy.Web.Models.Checkout
 {
 	public class CheckoutBillingAddressModel
 	{
-		public List<>
+		public List<AddressModel> ExistingAddresses { get; set; }
+
 	}
 }
--- OnlineStore/Web/Models/Common/AddressModel.cs
+++ OnlineStore/Web/Models/Common/AddressModel.cs
@@ -1,6 +1,39 @@
-﻿namespace GlideBuy.Web.Models.Common
+﻿using GlideBuy.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GlideBuy.Web.Models.Common
 {
-	public class AddressModel
+	public class AddressModel : BaseEntity
 	{
+		public string FirstName { get; set; }
+		
+		public string LastName { get; set; }
+
+		public string Email { get; set; }
+
+		public bool CountryEnabled { get; set; }
+		public int? CountryId { get; set; }
+		public string CountryName { get; set; }
+
+		public int? DefaultCountryId { get; set; }
+
+		public bool CityEnabled { get; set; }
+		public bool CityRequired { get; set; }
+		public string City { get; set; }
+
+		public bool StreetAddressEnabled { get; set; }
+		public bool StreetAddressRequired { get; set; }
+		public string Address1 { get; set; }
+
+		public bool StreetAddress2Enabled { get; set; }
+		public bool StreetAddress2Required { get; set; }
+		public string Address2 { get; set; }
+
+		public bool ZipCodeEnabled { get; set; }
+		public bool ZipCodeRequired { get; set; }
+		public string ZipCode { get; set; }
+
+		public IList<SelectListItem> AvailableCountries { get; set; }
+
 	}
 }
