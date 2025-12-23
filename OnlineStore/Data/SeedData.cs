d919bc68ea4b32f6e27fb05f3f816de431f1775a
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-12-23 20:46:46 +0300

    Add customer roles to the seed data

--- OnlineStore/Data/SeedData.cs
+++ OnlineStore/Data/SeedData.cs
@@ -1,12 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using GlideBuy.Models;
+﻿using GlideBuy.Core.Domain.Customers;
 using GlideBuy.Core.Domain.Seo;
+using GlideBuy.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace GlideBuy.Data
 {
 	public class SeedData
 	{
-		public static void EnsurePopulated(IApplicationBuilder app)
+		public static async Task EnsurePopulated(IApplicationBuilder app)
 		{
 			StoreDbContext context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<StoreDbContext>();
 
@@ -209,6 +210,43 @@ namespace GlideBuy.Data
 				);
 				context.SaveChanges();
 			}
+
+			await InstallCustomersAndRolesAsync(context);
+		}
+
+		private static async Task InstallCustomersAndRolesAsync(StoreDbContext context)
+		{
+			var crRegistered = new CustomerRole
+			{
+				Name = "Registered",
+				Active = true,
+				IsSystemRole = true,
+				SystemName = "Registered"
+			};
+			var crGuests = new CustomerRole
+			{
+				Name = "Guests",
+				Active = true,
+				IsSystemRole = true,
+				SystemName = "Guests"
+			};
+			var crVendors = new CustomerRole
+			{
+				Name = "Vendors",
+				Active = true,
+				IsSystemRole = true,
+				SystemName = "Vendors"
+			};
+
+			var customerRoles = new List<CustomerRole>
+			{
+				crRegistered,
+				crGuests,
+				crVendors
+			};
+
+			context.CustomerRoles.AddRange(customerRoles);
+			await context.SaveChangesAsync();
 		}
 	}
 }
