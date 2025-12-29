bc3c7c9ea86242d1fc55181e991e00c34539fedd
Muhammad Al-Tahan <me@mhd-altahan.com>
2025-12-29 21:34:00 +0300

    Add essential paging models

---
+++ OnlineStore/Web/UI/Paging/BasePageableModel.cs
@@ -0,0 +1,51 @@
+﻿using GlideBuy.Core;
+
+namespace GlideBuy.Web.UI.Paging
+{
+	public abstract record BasePageableModel : IPageableModel
+	{
+		public virtual void LoadPagedList<T>(IPagedList<T> pagedList)
+		{
+			FirstItemIndex = (pagedList.PageIndex * pagedList.PageSize) + 1;
+			// First case: The total count is smaller, so the last few items don't complete a full page.
+			// Second case: the current index + page size don't exceed the total count.
+			LastItemIndex = Math.Min(pagedList.TotalCount, (pagedList.PageIndex * pagedList.PageSize) + pagedList.PageSize);
+
+			HasNextPage = pagedList.HasNextPage;
+			HasPreviousPage = pagedList.HasPreviousPage;
+
+			PageNumber = pagedList.PageIndex;
+			PageSize = pagedList.PageSize;
+
+			TotalItems = pagedList.TotalCount;
+			TotalPages = pagedList.TotalPages;
+		}
+
+		public int PageIndex
+		{
+			get
+			{
+				if (PageNumber > 0)
+					return PageNumber - 1;
+
+				return 0;
+			}
+		}
+
+		public int PageNumber { get; set; }
+
+		public int PageSize { get; set; }
+
+		public int TotalItems { get; set; }
+
+		public int TotalPages { get; set; }
+
+		public int FirstItemIndex { get; set; }
+
+		public int LastItemIndex { get; set; }
+
+		public bool HasPreviousPage { get; set; }
+
+		public bool HasNextPage { get; set; }
+	}
+}
---
+++ OnlineStore/Web/UI/Paging/IPageableModel.cs
@@ -0,0 +1,28 @@
+﻿namespace GlideBuy.Web.UI.Paging
+{
+	public interface IPageableModel
+	{
+		/// <summary>
+		/// The index starts from 0.
+		/// </summary>
+		int PageIndex { get; }
+
+		int PageNumber { get; }
+
+		int PageSize { get; }
+
+		int TotalItems { get; }
+
+		int TotalPages { get; }
+
+		// The index of the first item in the CURRENT PAGE.
+		int FirstItemIndex { get; }
+
+		// The index of the last item in the CURRENT PAGE.
+		int LastItemIndex { get; }
+
+		bool HasPreviousPage { get; }
+
+		bool HasNextPage { get; }
+	}
+}
