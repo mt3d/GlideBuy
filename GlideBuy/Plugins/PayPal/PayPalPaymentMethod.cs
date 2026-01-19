bf49fa631324e4bd2c41a1903caa83f61cc8e60b
Muhammad Al-Tahan <me@mhd-altahan.com>
2026-01-19 09:52:24 +0300

    Add the initial PayPal payment method class

---
+++ GlideBuy/Plugins/PayPal/PayPalPaymentMethod.cs
@@ -0,0 +1,35 @@
+﻿using GlideBuy.Services.Payments;
+using GlideBuy.Services.Plugins;
+
+namespace GlideBuy.Plugins.PayPal
+{
+	public class PayPalPaymentMethod : BasePlugin, IPaymentMethod
+	{
+		public PaymentMethodType PaymentMethodType => throw new NotImplementedException();
+
+		public Task<OrderPaymentContext> GetPaymentInfoAsync(IFormCollection form)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<string> GetPaymentMethodDescriptionAsync()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Type GetPublicViewComponent()
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<ProcessPaymentResult> ProcessPaymentAsync(OrderPaymentContext processPaymentRequest)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<IList<string>> ValidatePaymentFormAsync(IFormCollection form)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
